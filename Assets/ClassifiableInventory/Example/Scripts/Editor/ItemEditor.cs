567b692f9ad8739ac3c58e8da7b324828bdb36d6
Maxim Kucherov <maximus.the0@gmail.com>
2020-04-10 19:48:43 +0300

    Preview for item assets.

---
+++ Assets/ClassifiableInventory/Example/Scripts/Editor/ItemEditor.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(Item))]
+public class ItemEditor : Editor
+{
+    private Sprite lastSprite = null;
+    private Texture2D lastPreview = null;
+
+    public override Texture2D RenderStaticPreview(string assetPath, Object[] subAssets, int width, int height)
+    {
+        var item = target as Item;
+        Texture2D result = null;
+        if (item != null && item.sprite != null)
+        {
+            result = TextureFromSprite(item.sprite);
+        }
+        if (result == null)
+        {
+            return base.RenderStaticPreview(assetPath, subAssets, width, height);
+        }
+        return result;
+    }
+
+    private Texture2D TextureFromSprite(Sprite sprite)
+    {
+        if (lastSprite == sprite && lastPreview != null)
+        {
+            return lastPreview;
+        }
+        lastPreview = null;
+        lastSprite = null;
+
+        var croppedTexture = new Texture2D((int)sprite.rect.width, (int)sprite.rect.height);
+        var pixels = sprite.texture.GetPixels((int)sprite.rect.x,
+                                                (int)sprite.rect.y,
+                                                (int)sprite.rect.width,
+                                                (int)sprite.rect.height);
+        var required = croppedTexture.width * croppedTexture.height;
+        var provided = pixels.Length;
+        if (provided < required)
+        {
+            Debug.LogWarning("[ItemEditor] Invalid size: " + provided.ToString() + " < " + required.ToString());
+            return null;
+        }
+        croppedTexture.SetPixels(pixels);
+        croppedTexture.Apply();
+
+        lastPreview = croppedTexture;
+        lastSprite = sprite;
+        return croppedTexture;
+    }
+}
