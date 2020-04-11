a68f64246a853f55e04895f4b6c66ade12191157
Maxim Kucherov <maximus.the0@gmail.com>
2020-04-11 15:23:20 +0300

    Add item types collection script/asset.

---
+++ Assets/ClassifiableInventory/Example/Scripts/ItemTypesCollection.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewItemTypesCollection", menuName = "Inventory/Item Types Collection")]
+public class ItemTypesCollection : ScriptableObject
+{
+    public ItemType[] itemTypes;
+}
